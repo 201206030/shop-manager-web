4e982d1ceb5128f31399b1bd4e38463124d16f9f
Administrator <Administrator@PC-201704181954>
2017-07-08 09:52:20 +0800

    create

---
+++ shop-manager-web/src/main/java/net/happyshop/web/ItemController.java
@@ -0,0 +1,25 @@
+package net.happyshop.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import net.happyshop.pojo.TbItem;
+import net.happyshop.service.ItemService;
+
+@Controller
+@RequestMapping("/item")
+public class ItemController {
+    
+    @Autowired
+    private ItemService itemService;
+    
+    @RequestMapping("/{itemId}")
+    @ResponseBody
+    public TbItem getItemById(@PathVariable long itemId){
+        return itemService.getItemById(itemId);
+    }
+
+}
